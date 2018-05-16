9fd15733f86365f0fe68973479865179ae1ba191
kimachesita <kimachesita@gmail.com>
2018-05-16 20:34:47 +0800

    environment set to prod

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/observable';
+//import { Observable } from 'rxjs/observable';
 import 'rxjs/add/operator/map';
 
 import { appConfig } from '../app.config';
